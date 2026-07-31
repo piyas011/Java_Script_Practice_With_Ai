f276a39e3c052aae14e521a6bf2fe128075f04e9
Piyas Ahmed <apiyas761@gmail.com>
2026-07-31 15:33:53 +0600

    Personal Information

---
+++ javaScript/Personal_Information.js
@@ -0,0 +1,25 @@
+// Task 1 : Personal Information
+// Store your name,age,and country in variables and print them.
+
+const firstName = "Piyas";
+const lastName = "Ahmed";
+let age = 21;
+const email = "apiyas221@gmail.com";
+let isStudent = false;
+const birthYear = 2005;
+const technology = ["HTML", "CSS", "JavaScript"];
+const country = "Bangladesh";
+
+console.log(
+  `
+ First Name  : ${firstName}
+ Last Name   : ${lastName}
+ Full Name   : ${firstName} ${lastName}
+ Age         : ${age}
+ Email       : ${email}
+ Is Student  : ${isStudent}
+ Birth Year  : ${birthYear}
+ Technology  : ${technology}
+ Country     : ${country}
+`,
+);
