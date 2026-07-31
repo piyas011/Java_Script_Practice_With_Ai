d244f8941cd9fe44f2c321f317618e5045085d42
Piyas Ahmed <apiyas761@gmail.com>
2026-07-31 16:00:36 +0600

    Even Or Odd Checker:

---
+++ javaScript/Even_Or_Odd_Checker.js
@@ -0,0 +1,11 @@
+// Even of odd checker
+// Check whether a number is even of odd.
+
+const number = 20;
+let result;
+if (number % 2 === 0) {
+  result = "Even Number";
+} else {
+  result = "Odd Number";
+}
+console.log(`${number} = ${result}`);
