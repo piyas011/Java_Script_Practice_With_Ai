db29d798bef51984d54dc602d920d266ff183568
Piyas Ahmed <apiyas761@gmail.com>
2026-08-01 10:41:18 +0600

    Factorial Number

---
+++ javaScript/Factorial.js
@@ -0,0 +1,10 @@
+// Task 17 | Factorial
+// Find the factorial of a number.
+
+const number = 5;
+let factorialNumber = 1;
+
+for (let i = 1; i <= number; i++) {
+  factorialNumber *= i;
+}
+console.log(factorialNumber);
