5974ddfec46147d4e2bf24e95b91743772a53962
Piyas Ahmed <apiyas761@gmail.com>
2026-08-01 10:16:02 +0600

    Multiplication Table

---
+++ javaScript/Multiplication_Table.js
@@ -0,0 +1,8 @@
+// Task 14 : Multiplication Table
+// Print the multiplication table of a given number.
+
+const number = 7;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(` ${i} * ${number} = ${number * i}`);
+}
